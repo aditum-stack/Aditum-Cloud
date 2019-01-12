e40371832975890f09c603d9ec9701e98bb3ade2
kevinten10 <596823919@qq.com>
2019-01-12 19:05:44 +0800

    update eureka server

---
+++ Eureka-Server/src/main/java/com/ten/aditum/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.ten.aditum.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server 服务发现中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
