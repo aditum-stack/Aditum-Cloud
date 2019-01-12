50b44e184230744c7074db9a87125c6a12b584c6
kevinten10 <596823919@qq.com>
2019-01-12 19:19:17 +0800

    update eureka client

--- Eureka-Server/src/main/java/com/ten/aditum/cloud/gatewayzuul/EurekaServerApplication.java
+++ Eureka-Server/src/main/java/com/ten/aditum/cloud/gatewayzuul/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.ten.aditum.cloud.eurekaserver;
+package com.ten.aditum.cloud.gatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
