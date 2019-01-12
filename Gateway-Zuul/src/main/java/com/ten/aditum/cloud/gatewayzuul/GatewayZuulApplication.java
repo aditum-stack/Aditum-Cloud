ad93f0cdd8c3281e7fcfaead7ebb10dc53a25e41
kevinten10 <596823919@qq.com>
2019-01-12 19:18:12 +0800

    add zuul gateway

---
+++ Gateway-Zuul/src/main/java/com/ten/aditum/cloud/gatewayzuul/GatewayZuulApplication.java
@@ -0,0 +1,16 @@
+package com.ten.aditum.cloud.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Zuul API 网关服务
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+}
